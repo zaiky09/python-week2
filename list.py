fa7ac76c1b040c012c35e12d9879971bc444483d
zaiky09 <zamilmozamil@outlook.com>
2024-11-13 15:39:25 +0300

    common methods on lists

---
+++ list.py
@@ -0,0 +1,37 @@
+# 1. Create empty list
+
+my_list = []
+
+# 2. Append elements to the list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Insert 15 at index 1
+
+my_list.insert(1, 15)
+
+# 4. Extend my_list with another list
+
+list = [50, 60, 70]
+
+my_list.extend(list)
+
+# 5. Remove the last element from my_list
+
+del my_list[-1]
+
+#6. Sort my_list in ascending order
+
+my_list.sort()
+
+# 7. Find and print index of value 30 in my_list
+
+index_30 = my_list.index(30)
+
+print (index_30)
+
+
+
